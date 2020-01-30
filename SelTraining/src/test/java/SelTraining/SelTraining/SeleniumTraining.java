f6c00176d973b118ed2b917ed15589fe22124e62
dineshkumarlok <60347486+dineshkumarlok@users.noreply.github.com>
2020-01-30 12:30:44 +0530

    Update SeleniumTraining.java

--- SelTraining/src/test/java/SelTraining/SelTraining/SeleniumTraining.java
+++ SelTraining/src/test/java/SelTraining/SelTraining/SeleniumTraining.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumTraining {
 
 	public void selTraining(){
+		//Driver Initialization
 		System.setProperty("webdriver.chrome.driver", "D://Dineshkumar//07_Personal//AutomationLearn//SelTraining//Resource//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
