90a044a73791b36cbbe4f254b339a59c7daee1e6
dineshkumarlok <dineshkumar.lok@gmail.com>
2020-01-30 12:05:22 +0530

    Automation Trials

---
+++ SelTraining/src/test/java/SelTraining/SelTraining/SeleniumTraining.java
@@ -0,0 +1,13 @@
+package SelTraining.SelTraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTraining {
+
+	public void selTraining(){
+		System.setProperty("webdriver.chrome.driver", "D://Dineshkumar//07_Personal//AutomationLearn//SelTraining//Resource//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+}
